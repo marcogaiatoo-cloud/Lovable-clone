0ad45bcaa1a1d41f1d3f98068aa065115b93d52b
marcogaiatoo-cloud <marcogaiatoo@gmail.com>
2025-12-19 01:30:53 +0000

    Adicionando arquivo script.js com funcionalidades básicas

---
+++ script.js
@@ -0,0 +1,7 @@
+// Este arquivo script.js será utilizado para adicionar interatividade e funcionalidades ao site.
+// Conecte este arquivo ao arquivo index.html utilizando a tag <script>.
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Script.js conectado e funcionando corretamente!");
+    // Adicione aqui os eventos e funcionalidades do site.
+});
